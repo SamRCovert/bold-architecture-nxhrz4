31f0c4808815f64de0a439bd1bed75f9e0ea4df5
Samuel Covert <samuelrcovert@gmail.com>
2025-09-05 07:49:59 +0000

    Changed "Continue" to "Log In" on home screen

--- src/App.js
+++ src/App.js
@@ -498,7 +498,7 @@ export default function App() {
               if (firstName && lastName) setShowDirectory(true);
             }}
           >
-            Continue
+            Log In
           </button>
         </div>
       </div>
